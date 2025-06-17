43567fe2bda3e3a448e081e63b55d0fa8f074b40
robertkutwa <robertkutwa0@gmail.com>
2025-06-17 09:41:45 +0300

    chatter box

--- server/app.py
+++ server/app.py
@@ -1,26 +1,53 @@
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
 CORS(app)
-migrate = Migrate(app, db)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/messages')
-def messages():
-    return ''
+# ROUTES
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# GET all messages
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([m.to_dict() for m in messages]), 200
+
+# POST new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        username=data['username'],
+        body=data['body']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+# PATCH update message
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+# DELETE message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,15 @@
-#!/usr/bin/env python3
-
-from random import choice as rc
-
-from faker import Faker
-
 from app import app
 from models import db, Message
 
-fake = Faker()
-
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
-
-def make_messages():
-
+with app.app_context():
+    print("Clearing existing messages...")
     Message.query.delete()
+
+    print("Seeding database...")
+    msg1 = Message(username='Ian', body='Hello, World!')
+    msg2 = Message(username='Robert', body='Chatterbox is alive!')
     
-    messages = []
+    db.session.add_all([msg1, msg2])
+    db.session.commit()
 
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
-
-    db.session.add_all(messages)
-    db.session.commit()        
-
-if __name__ == '__main__':
-    with app.app_context():
-        make_messages()
+    print("Seeding complete!")
